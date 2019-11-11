b4eade6ef60f06c245edc96cde4de589eed172ea
Kili <kilimanjaro.a2@gmail.com>
2019-11-11 21:27:33 +0900

    Main Scene

---
+++ Assets/JslibCaller.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Runtime.InteropServices;
+
+public class JslibCaller : MonoBehaviour {
+    [DllImport("__Internal")]
+    private static extern void FileImporterCaptureClick();
+    [SerializeField]
+    private Text clickInformationText;
+    [SerializeField]
+    private Text fileInformationText;
+    private int clickCount = 0;
+
+    public void OnButtonClicked()
+    {
+        clickInformationText.text = ++clickCount + "回ボタンがクリックされました！";
+        FileImporterCaptureClick();
+    }
+
+    public void FileSelected(string url)
+    {
+        fileInformationText.text = url + "が読み込まれました";
+    }
+}
